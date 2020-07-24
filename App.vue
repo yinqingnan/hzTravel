f4b3bfb4e74391be4bffdcd1669710f72d22969b
yinqingnan <754417575@qq.com>
2020-07-24 16:13:44 +0800

    yqn

--- App.vue
+++ App.vue
@@ -1,18 +1,19 @@
 <script>
 	export default {
 		onLaunch: function() {
-			console.log('App Launch')
+			// console.log('App Launch')
 		},
 		onShow: function() {
-			console.log('App Show')
+			// console.log('App Show')
 		},
 		onHide: function() {
-			console.log('App Hide')
+			// console.log('App Hide')
 		}
 	}
 </script>
 
 <style>
+	/* 引入colorUI关键css */
 	@import "static/ColorUI/main.css";
 	@import "static/ColorUI/icon.css";
 	/*每个页面公共css */
